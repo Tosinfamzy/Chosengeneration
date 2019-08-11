34c2e3e1752aa441146c0388db280720e56e16be
Tosinfamzy <andy@centu.co.uk>
2019-08-11 21:19:27 +0100

    Homepage with footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,16 +1,16 @@
 <footer class="footer">
     <div class="footer__logo_box">
-        <img src="/img/logo-full.png" alt="Full Logo" class="footer_logo" height="150px">
+        <img src="/img/logo-full.png" alt="Full Logo" class="footer__logo">
     </div>
     <div class="row">
         <div class="col-1-of-2">
             <div class="footer__navigation">
                 <ul class="footer__list">
-                    <li class="footer__item"><a href="#" class="footer__link">Company</a></li>
-                    <li class="footer__item"><a href="#" class="footer__link">Company</a></li>
-                    <li class="footer__item"><a href="#" class="footer__link">Company</a></li>
-                    <li class="footer__item"><a href="#" class="footer__link">Company</a></li>
-                    <li class="footer__item"><a href="#" class="footer__link">Company</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">HOME</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">ABOUT</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">GLOWFEST</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">EVENTS</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">CONTACT</a></li>
                 </ul>
             </div>
         </div>
