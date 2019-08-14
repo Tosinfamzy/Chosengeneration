f074e97eeabd8b1a8c601c677b4107fc8fe8c5e9
Tosinfamzy <andy@centu.co.uk>
2019-08-14 19:50:26 +0100

    Footer navigation

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,22 +1,30 @@
 <footer class="footer">
-    <div class="footer__logo_box">
-        <img src="/img/logo-full.png" alt="Full Logo" class="footer__logo">
+    <div class="footer__logo-box">
+
+        <picture class="footer__logo">
+            <source srcset="img/logo-icon.png 1x, img/logo-icon.png 2x"
+                    media="(max-width: 37.5em)">
+            <img srcset="img/logo-icon.png 1x, img/logo-icon.png 2x" alt="Full logo" src="img/logo-icon.png">
+        </picture>
+
+
+
     </div>
     <div class="row">
         <div class="col-1-of-2">
             <div class="footer__navigation">
                 <ul class="footer__list">
-                    <li class="footer__item"><a href="#" class="footer__link">HOME</a></li>
-                    <li class="footer__item"><a href="#" class="footer__link">ABOUT</a></li>
-                    <li class="footer__item"><a href="#" class="footer__link">GLOWFEST</a></li>
-                    <li class="footer__item"><a href="#" class="footer__link">EVENTS</a></li>
-                    <li class="footer__item"><a href="#" class="footer__link">CONTACT</a></li>
-                </ul>
+                    <li class="footer__item"><a href="#" class="footer__link">About </a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Glofest</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Events</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Contact</a></li>
+                    </ul>
             </div>
         </div>
         <div class="col-1-of-2">
             <p class="footer__copyright">
-                Built by <a href="#" class="footer__link">Tosin Famurewa</a>
+                Built by <a href="#" class="footer__link">Oluwatosin Famurewa</a>.
+
             </p>
         </div>
     </div>
